98b2c3a75a7683ab7382b3d809cb33c84f599a25
KevinZuo2002 <124839450+KevinZuo2002@users.noreply.github.com>
2024-02-08 17:28:55 +0000

    Delete tango_with_django directory

--- tango_with_django/rango/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from rango.models import Category, Page
-from rango.models import Employee, Department
-admin.site.register(Category)
-admin.site.register(Page)
-admin.site.register(Employee)
-admin.site.register(Department)
-# Register your models here.
